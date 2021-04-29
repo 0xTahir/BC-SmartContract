d0706ce30cc48dd416f1a4bc9549a523956acb9b
Tahir Naveed <tahir_naveed@live.com>
2021-04-29 12:00:32 -0400

    Add files via upload

---
+++ Contracts/MyContract.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.24;
+
+/**
+ * Sample Smart Contract
+ * Store & retrieve value in a variable
+ */
+
+contract MyConytract {
+
+    string message;
+
+    constructor() public{
+        message = "Smart Contract by Tahir.";
+        
+    }
+    
+    function store(string _value) public {
+        message = _value;
+    }
+
+
+    function retrieve() public view returns (string){
+        return message;
+    }
+}
